bfa3f7d69e321905e1c45e69f3c15559eebfe7ed
Priyam Maity <45975028+Priyam31@users.noreply.github.com>
2022-12-19 18:35:50 +0530

    Update about_class.py

--- about_class.py
+++ about_class.py
@@ -1,3 +1,5 @@
 # let us understand the functioning of Object Oriented Programming by playing the game "Age of Dragons"
 
-def get_soldier_dps(soldier)
+def get_soldier_dps(soldier):
+  return soldier["damage"] * soldier["attacks_per_second"]
+
